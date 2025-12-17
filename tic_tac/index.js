9f9c0c3792c6703fabffeaa2226ba8e1f9721445
Gani25 <memonabdulgani3d@gmail.com>
2025-12-17 16:15:14 +0530

    JS

--- tic_tac/index.js
+++ tic_tac/index.js
@@ -1,6 +1,8 @@
 let boxes = document.querySelectorAll(".box"); // array of node elements
 let gameInfo = document.querySelector(".game-info");
 
+let newGameBtn = document.querySelector(".btn");
+
 let currentPlayer;
 let gameGrid;
 
@@ -10,9 +12,35 @@ function initialize() {
 
   boxes.forEach((box, index) => {
     box.innerText = "";
+    box.style.pointerEvents = "all";
     box.classList = `box box${index + 1}`;
   });
+
+  newGameBtn.classList.remove("active");
   gameInfo.innerText = `Current Player - ${currentPlayer}`;
 }
 
 initialize();
+
+function swapPlayer() {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+  gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+function handleClick(index) {
+  if (gameGrid[index] == "") {
+    boxes[index].innerText = currentPlayer;
+    gameGrid[index] = currentPlayer;
+    boxes[index].style.pointerEvents = "none";
+    swapPlayer();
+  }
+}
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    handleClick(index);
+  });
+});
